36ac63d259f6b52730f99407e4a86f6cc86bdc49
gurpreet <apixoninfo@gmail.com>
2015-10-12 01:15:38 +0530

    python list showing possible routes

---
+++ routes.py
@@ -0,0 +1,34 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+class unique_element:
+    def __init__(self,value,occurrences):
+        self.value = value
+        self.occurrences = occurrences
+
+def perm_unique(elements):
+    eset=set(elements)
+    listunique = [unique_element(i,elements.count(i)) for i in eset]
+    u=len(elements)
+    return perm_unique_helper(listunique,[0]*u,u-1)
+
+def perm_unique_helper(listunique,result_list,d):
+    if d < 0:
+        yield tuple(result_list)
+    else:
+        for i in listunique:
+            if i.occurrences > 0:
+                result_list[d]=i.value
+                i.occurrences-=1
+                for g in  perm_unique_helper(listunique,result_list,d-1):
+                    yield g
+                i.occurrences+=1
+
+
+
+
+string_input = raw_input() ## ABC
+input_list =  list(string_input) ## [A,B,C]
+    
+a = list(perm_unique(input_list)) ## (perm_unique([A,B,C]))
+print (a)
+print 'No. of possible unique routes : %d' % len(a)
